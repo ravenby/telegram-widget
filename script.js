bacc642bebbf611fb5f3497ea0fcd01d3f286e1e
ravenby <84452706+ravenby@users.noreply.github.com>
2025-07-27 18:24:22 +0300

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-define(['jquery'], function($) {
-  return function() {
-    this.init = function() {
-      this.system().events.on('contacts:card.open deals:card.open', function(card) {
-        card.phone_fields.forEach(function(field) {
-          let raw = field.raw;
-          let digits = raw.replace(/\D/g,'');
+define(['jquery'], function ($) {
+  return function () {
+    this.init = function () {
+      this.system().events.on('contacts:card.open leads:card.open', function (card) {
+        card.phone_fields.forEach(function (field) {
+          const raw = field.raw;
+          const digits = raw.replace(/\D/g, '');
+          let formatted = '';
           if (digits.startsWith('80') && digits.length === 11) {
-            digits = '375' + digits.slice(2);
+            formatted = '375' + digits.slice(2);
           } else if (digits.startsWith('375') && digits.length === 12) {
-            // ok
-          } else return;
-          if (field.node.find('.btn-tg').length) return;
+            formatted = digits;
+          } else {
+            return;
+          }
+
+          if (field.node.find('.tg-btn').length) return;
+
           const btn = $('<button>')
-            .addClass('btn-tg')
-            .text('Telegram')
-            .css({marginLeft:'8px'})
-            .on('click', () => { window.open(`tg://resolve?phone=${digits}`); });
+            .addClass('tg-btn')
+            .text('Написать в Telegram')
+            .css({ marginLeft: '8px' })
+            .on('click', function () {
+              window.open(`tg://resolve?phone=${formatted}`);
+            });
+
           field.node.append(btn);
         });
       });
