94428da7e0ca39c1944e2062f4c4e36a619af462
ravenby <84452706+ravenby@users.noreply.github.com>
2025-07-27 18:32:33 +0300

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-define(['jquery'], function ($) {
-  return function () {
-    this.init = function () {
-      this.system().events.on('contacts:card.open leads:card.open', function (card) {
-        card.phone_fields.forEach(function (field) {
+define(['jquery'], function($) {
+  return function() {
+    this.init = function() {
+      this.system().events.on('contacts:card.open leads:card.open', function(card) {
+        if (!card.phone_fields) return;
+
+        card.phone_fields.forEach(function(field) {
           const raw = field.raw;
           const digits = raw.replace(/\D/g, '');
           let formatted = '';
+
           if (digits.startsWith('80') && digits.length === 11) {
             formatted = '375' + digits.slice(2);
           } else if (digits.startsWith('375') && digits.length === 12) {
@@ -20,7 +23,7 @@ define(['jquery'], function ($) {
             .addClass('tg-btn')
             .text('Написать в Telegram')
             .css({ marginLeft: '8px' })
-            .on('click', function () {
+            .on('click', function() {
               window.open(`tg://resolve?phone=${formatted}`);
             });
 
